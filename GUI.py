33809a148f02dc20ced6c342805e25dd6795ac01
defneeyilmaz <defneyilmaz210@gmail.com>
2024-05-05 22:49:47 +0300

    label function and title added

--- GUI.py
+++ GUI.py
@@ -8,12 +8,16 @@ class GUI(customtkinter.CTk):
 
         self.geometry("500x500")
         self.title("Best Hotels For You")
-        customtkinter.set_appearance_mode("light")
+        self.label("Best Hotels For You!", 0.5, 0.05, 40)
+        customtkinter.set_appearance_mode("dark")
 
     def button(self, name, relx, rely):
         button = customtkinter.CTkButton(master=self, text=name)
-
         button.place(relx=relx, rely=rely, anchor=CENTER)
+
+    def label(self, name, relx, rely, fontsize):
+        label = customtkinter.CTkLabel(master=self, text=name, font=('Helvetica', fontsize), text_color="#DF3C5F", fg_color="#224193",corner_radius=8)
+        label.place(relx=relx, rely=rely, anchor=CENTER)
 
     def loop(self):
         self.mainloop()
